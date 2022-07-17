8abe2f7734485aebe750d8b32f0a5e3688d2c84a
Jianend <2110179446@qq.com>
2022-07-17 23:40:00 +0800

    a

--- paix,/.py
+++
@@ -1,7 +0,0 @@
-#排序
-def paixu(list):
-    for i in range(len(list)):
-        for j in range(len(list)-1):
-            if list[j]>list[j+1]:
-                list[j],list[j+1]=list[j+1],list[j]
-    return list
